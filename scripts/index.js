6108ca606139d3e531f2e10696d69197eef93713
vadivel <gct.vadivel@gmail.com>
2016-02-27 10:22:08 -0600

    test stuff

--- scripts/index.js
+++ scripts/index.js
@@ -2,12 +2,10 @@ new gnMenu( document.getElementById( 'gn-menu' ) );
 /*
 * Socket communication
 */
-var ip = '192.168.1.125', // node server ip
-port = ':8080';
 
 $.ready(function() {
     document.getElementById('debug').innerHTML = document.getElementById('debug').innerHTML + "<br/>before calling sockjs";
-    var sockjs_url = 'http://192.168.1.125:9999/echo';
+    var sockjs_url = 'http://192.168.0.37:9999/echo';
     var sockjs = new SockJS(sockjs_url);
     document.getElementById('debug').innerHTML = document.getElementById('debug').innerHTML + "<br/>device ready and sockjs in action";
     var sockjsdiv  = $('#sockjs');
